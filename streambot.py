b9b49c2f5f125541d03d605f9a2aec94d4bbacd0
dik-dik <dik-dik@users.noreply.github.com>
2017-05-09 01:38:54 -0500

    Update streambot.py

--- streambot.py
+++ streambot.py
@@ -18,9 +18,10 @@ def load_data():
     global prevcounts
     prevcounts = json.load(open('prevcounts.json', 'r'))
     
-    print "Loaded the data"
+    print "Loading previous counts"
     
 def get_counts():
+    global countresponse
     print "Updating counts...."
     # call twitter api for each
     potus = api.get_user('potus')
@@ -31,6 +32,8 @@ def get_counts():
     data['rdtcount'] = rdt.statuses_count - data['rdtnum']
     data['vpcount'] = vp.statuses_count - data['vpnum']
     
+    countresponse = "<" + data['potuslink'] + "|*POTUS*> " + str(data['potuscount']) + "       <" + data['rdtlink'] + "|*RDT*> " + str(data['rdtcount']) + "       <" + data['vplink'] + "|*VP*> " + str(data['vpcount']) + "       <http://realcount.club/|more>"
+    
     prevcounts['vp'] = data['vpcount']
     prevcounts['rdt'] = data['rdtcount']
     prevcounts['potus'] = data['potuscount']
@@ -38,9 +41,13 @@ def get_counts():
     
     print "POTUS "+str(data['potuscount'])+"     RDT: "+str(data['rdtcount'])+"     VP:"+str(data['vpcount'])
 
-def post_message(channel, response):
+def post_message(channel, response, username=None, pic=None):
+    if username is None:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    else:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=False, username=username, icon_url=pic, unfurl_media=True)
     # print(response)
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    
     
 #load secrets
 f = json.load(open('secrets.json', 'r'))
@@ -63,8 +70,41 @@ slack_client = SlackClient(slack_token)
 BOT_NAME = 'autocountbot'
 
 
+
 # This is the listener, resposible for receiving data
-class StdOutListener(tweepy.StreamListener):
+class StdOutListener(tweepy.StreamListener):    
+    def on_delete(self, status_id, user_id):
+        if user_id == 818910970567344128:
+            response = "VP DELETION!!!"
+            channel = "#alert"
+        elif user_id == 25073877:
+            response = "RDT DELETION!!!"
+            channel = "#alert"
+        elif user_id == 822215679726100480:
+            response = "POTUS DELETION!!!"
+            channel = "#alert"
+        elif user_id == 836598396165230594:
+            channel = "#test"
+            response = "predickit DELETION!!!"
+            
+        post_message(channel,response)
+        print response
+        get_counts()
+        post_message(channel,countresponse)
+        
+        if user_id == 818910970567344128:
+            channel = "#general"
+        elif user_id == 25073877:
+            channel = "#general"
+        elif user_id == 822215679726100480:
+            channel = "#general"
+        elif user_id == 836598396165230594:
+            return
+        
+        post_message(channel,response+"\n"+countresponse)
+        
+        return
+    
     def on_status(self, status):
         channel1 = "#general"
         channel2 = "#twitteralert"
@@ -73,34 +113,33 @@ class StdOutListener(tweepy.StreamListener):
         url = "https://twitter.com/"+status.user.screen_name+"/status/"+str(status.id)
         
         if status.truncated:
-            head, sep, tail = status.extended_tweet['full_text'].partition('http')
+            vpresponse, sep, tail = status.extended_tweet['full_text'].partition('http')
         else:
-            head = status.text
+            vpresponse, sep, tail = status.text.partition('http')
         
-        response = "*@"+status.user.screen_name+" tweet!* "+url
-        vpresponse = "*@"+status.user.screen_name+" tweet!* "+head
+        #response = "*@"+status.user.screen_name+" tweet!* "+url
+        #vpresponse = "*@"+status.user.screen_name+" tweet!* "+head
         #reload the data
         load_data()
         
         #get the current counts
         get_counts()
         
-        countresponse = "\n<" + data['potuslink'] + "|*POTUS*> " + str(data['potuscount']) + "       <" + data['rdtlink'] + "|*RDT*> " + str(data['rdtcount']) + "       <" + data['vplink'] + "|*VP*> " + str(data['vpcount']) + "       <http://realcount.club/|more>"
-        
-        response += countresponse
-        vpresponse += countresponse
+        at_name = "@" + status.user.screen_name+ " tweet!"
+        response = url + "\n" + countresponse
+        vpresponse = vpresponse + "\n" + countresponse
         
         if status.user.screen_name != 'predickit':
-            post_message(channel2, response)
+            post_message(channel2, response, at_name, status.user.profile_image_url)
             if status.user.screen_name == 'VP':
-                post_message(channel1, vpresponse)
+                post_message(channel1, vpresponse, at_name, status.user.profile_image_url)
             else:
-                post_message(channel1, response)
+                post_message(channel1, response, at_name, status.user.profile_image_url)
         if status.user.screen_name == 'predickit':
-            post_message(testchannel, vpresponse)
+            post_message(testchannel, response, at_name, status.user.profile_image_url)
         
-        print response       
-        
+        print at_name
+        #print response        
     def on_error(self, status):
         print status
 
